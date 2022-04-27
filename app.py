3ecff892489badaca5cc742981c49f145ce062ee
Nico Rossi <nicocrossi2000@gmail.com>
2022-04-27 02:30:01 -0500

    added way to delete event

--- app.py
+++ app.py
@@ -1,49 +1,16 @@
-from typing import List
+from datetime import datetime
+from typing import List, Tuple
+
 from flask import Flask, render_template, request, redirect, url_for
-# import logging
 import schedule
 import time
 import threading
 import socket
 
-import os
-
 app = Flask(__name__)
 
-jobs: List[schedule.Job] = []
-
-# logger = logging.getLogger("werkzeug")
-# logger.setLevel(logging.ERROR)
-
-socket_path = "/home/nicorossi/csprojects/ELEC327-Webserver/ipc.sock"
-
-try:
-    os.unlink(socket_path)
-except OSError:
-    pass
-
-sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-
-sock.connect(socket_path)
-
-ip = "192.168.1.1"
-tst = bytearray(len(ip) + 1)
-
-# for idx, c in ip:
-#     tst[idx] = 
-
-tst = bytes(ip, 'utf-8')
-
-
-# sock.send(tst)
-
-ba = bytearray(5)
-ba[0] = 1
-ip = [192, 168, 1, 1]
-for i in range(1, 5):
-    ba[i] = ip[i - 1]
-
-print(ba)
+# [(time, str rep, job object), ...]
+jobs: List[Tuple[datetime.time, str, schedule.Job]] = []
 
 
 def send_ip(ip: str):
@@ -61,8 +28,12 @@ def light_on():
     # TODO: Tell the msp430 to turn on the light
 
 
-def jobs_sort(job: schedule.Job):
-    return job.at_time if job.at_time else 0
+def jobs_sort(job: Tuple[datetime.time, str, schedule.Job]):
+    return job[0]
+
+
+def pretty_jobs(ugly_jobs: List[Tuple[datetime.time, str, schedule.Job]]):
+    return [j[1] for j in ugly_jobs]
 
 
 def run_continuously(interval=1):
@@ -91,23 +62,45 @@ def hello_world():
 
 @app.route("/control")
 def control():
-    return render_template("control.html")
+    return render_template("control.html", set_events=pretty_jobs(jobs))
 
 
 @app.route("/control_form", methods=['POST'])
 def control_form():
     option = request.form.get("options")
     if option == "set_event":
-        print("Setting event")
-        e_option = request.form.get("e_options")
-        if e_option == "e_light_on":
-            pass
-            print(f"Scheduling on at {request.form.get('e_time')}")
-            jobs.append(schedule.every().day.at(request.form.get("e_time")).do(light_on))
-        elif e_option == "e_light_off":
-            pass
-            print(f"Scheduling off at {request.form.get('e_time')}")
-            jobs.append(schedule.every().day.at(request.form.get("e_time")).do(light_off))
+        print("Managing events")
+        manage_e_option = request.form.get("e_mod_type")
+        if manage_e_option == "add_event":
+            print("Adding event")
+            e_option = request.form.get("e_options")
+            if e_option == "e_light_on":
+                pass
+                print(f"Scheduling on at {request.form.get('e_time')}")
+                job = schedule.every().day.at(request.form.get("e_time")).do(light_on)
+                jobs.append((job.at_time, f"On at {job.at_time}", job))
+                jobs.sort(key=jobs_sort)
+            elif e_option == "e_light_off":
+                pass
+                print(f"Scheduling off at {request.form.get('e_time')}")
+                job = schedule.every().day.at(request.form.get("e_time")).do(light_off)
+                jobs.append((job.at_time, f"Off at {job.at_time}", job))
+                jobs.sort(key=jobs_sort)
+        elif manage_e_option == "remove_event":
+            print("Deleting event")
+            to_delete_str = request.form.get("events")
+            print(to_delete_str)
+
+            to_delete = None
+            for j in jobs:
+                if j[1] == to_delete_str:
+                    to_delete = j
+                    break
+
+            if to_delete:
+                jobs.remove(to_delete)
+                schedule.cancel_job(to_delete[2])
+
     else:
         print("Making change")
         if option == "light_on":
@@ -120,8 +113,6 @@ def control_form():
 
 @app.route("/events")
 def events():
-    # TODO: make jobs cancellable
-    jobs.sort(key=jobs_sort)
     return str(jobs)
 
 
